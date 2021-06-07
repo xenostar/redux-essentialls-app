451352c6afc7051689b15a0b90eee47316a0cb38
xenostar <bryan.a.leewood@gmail.com>
2021-06-06 21:38:26 -0700

    main Adding dates and sorting posts in reverse chronological order.

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,14 +3,21 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { PostAuthor } from 'features/posts/PostAuthor'
+import { TimeAgo } from 'features/posts/TimeAgo'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
 
-  const renderedPosts = posts.map((post) => (
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <PostAuthor userId={post.user} />
+      <TimeAgo timestamp={post.date} />
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { PostAuthor } from 'features/posts/PostAuthor'
+import { TimeAgo } from 'features/posts/TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -24,6 +25,7 @@ export const SinglePostPage = ({ match }) => {
       <article className="post">
         <h2>{post.title}</h2>
         <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
         <p className="post-content">{post.content}</p>
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
