cbf4890aaafabf8bd84f538aeb85dc8aea88f309
xenostar <bryan.a.leewood@gmail.com>
2021-06-07 01:04:39 -0700

    main Fixing errors.

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,5 @@
-import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { client } from 'api/client'
-
-const reactions = {
-  reactions: {
-    thumbsUp: 0,
-    hooray: 0,
-    heart: 0,
-    rocket: 0,
-    eyes: 0,
-  },
-}
 
 const initialState = {
   posts: [],
